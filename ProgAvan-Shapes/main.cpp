bde4e5f5c44e3c18c2daf7977b97699000ff6498
Nacho Martinez <anxooxna@me.com>
2019-01-09 10:24:10 +0100

    Initial Commit

---
+++ ProgAvan-Shapes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProgAvan-Shapes
+//
+//  Created by Nacho Martinez on 09/01/2019.
+//  Copyright © 2019 Nacho Martinez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
